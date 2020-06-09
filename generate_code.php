574a12de6bd31b9377069dbe529239626c0b0636
JagMolar <juana.garmue@educa.jcyl.es>
2020-06-09 11:11:26 +0200

    adding autoload

--- generate_code.php
+++ generate_code.php
@@ -1,4 +1,16 @@
 <?php 
+require  ' autoload.php ' ; //Cargamos las ayudas para cloudinary
+require  ' Helpers.php ' ; // opcional para usar los métodos auxiliares cl_image_tag y cl_video_tag
+
+//CLOUDINARY_URL=cloudinary://765615988784445:7fDja_bjq-Nikujy5DCo9pK7FvE@jagmolar
+
+\Cloudinary::config(array( 
+    "cloud_name" => "jagmolar", 
+    "api_key" => "765615988784445", 
+    "api_secret" => "7fDja_bjq-Nikujy5DCo9pK7FvE", 
+    "secure" => true
+  ));
+
 //Para tratar archivos si queremos modificar la recepción de datos
 //$codFiles = "files/";//Carpeta de destino
 //opendir($codFiles);//Abrir directorio
@@ -14,13 +26,11 @@
 if(isset($_POST) && !empty($_POST)) {
     //Si se reciben datos, se llama a la libreria
     include('library/phpqrcode/qrlib.php'); 
-    // $codesDir = "codes/"; //Carpeta de destino de las imágenes creadas  
+    $codesDir = \Cloudinary\Uploader::upload('$qr'); //Carpeta de destino de las imágenes creadas  
     $codeFile = date('d-m-Y-h-i-s').'.png';//se almacenan por fecha, asegurando sean únicos
     //El método png crea el QR
-    // QRcode::png($_POST['formData'], $codesDir.$codeFile, $_POST['ecc'], $_POST['size']); 
-    // QRcode::png($_POST['formData'], $codeFile, $_POST['ecc'], $_POST['size']); 
-    // echo '<img class="img-thumbnail" src="'.$codeFile.'" />';
-    echo '<h1>Imagen'.$codeFile.'</h1>';
+    $qr = QRcode::png($_POST['formData'], $codesDir.$codeFile, $_POST['ecc'], $_POST['size']); 
+    echo '<img class="img-thumbnail" src="'.$codesDir.$codeFile.'" />';
 } else {
     header('location:./');
 }
